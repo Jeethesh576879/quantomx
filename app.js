4331d4efa82e2d2e3e0be378b9d937361ecf01bd
blue berrry <jeetudzaa01@gmail.com>
2025-07-31 19:12:47 +0530

    Add files via upload
    Uploaded starter files

---
+++ app.js
@@ -0,0 +1,8 @@
+function connectWallet() {
+  alert("🔐 Wallet connection coming in next step.");
+}
+
+function acceptTerms() {
+  document.getElementById("terms").style.display = "none";
+  document.getElementById("app").style.display = "block";
+}
